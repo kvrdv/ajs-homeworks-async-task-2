dd9d80682a1647d39a679381a3d064c1ae09954c
Semyon Kovardaev <s.kovardaev@gmail.com>
2020-10-10 18:22:10 +0300

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,10 @@
+import GameSavingLoader from './GameSavingLoader';
+
+(async () => {
+  try {
+    const playerGameSaving = await GameSavingLoader.load();
+    console.log(playerGameSaving);
+  } catch (error) {
+    console.log(error);
+  }
+})();
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-
+npm
 const path = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
